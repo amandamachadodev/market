dc8209e204823d04bc7894f6ffe70702a541977f
mandioquynha <machado.amandag@gmail.com>
2023-06-01 11:40:10 -0300

    feat: add styled components

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,32 @@
 import React from "react";
-import "../Header/Header.css";
+// import "../Header/Header.css";
 import { AiOutlineMenu, AiOutlineUser } from 'react-icons/ai';
 import { useState } from "react";
+import styled from 'styled-components';
 
 export function Header() {
+
+    const TagHeader = styled.header`
+        display: flex;
+        justify-content: space-between;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 9%;
+        background-color: #fff159;
+
+        .Header-icon {
+            margin-top: 1%;
+            padding: 4%;
+        }
+
+        p {
+            margin-top: 7%;
+            color: rgb(0, 153, 255);
+        }
+    `
+
     const [menuShow, setMenuShow] = useState(false);
 
     function handleClick() {
@@ -12,10 +35,10 @@ export function Header() {
     }
 
     return(
-        <header className="Header">
+        <TagHeader className="Header">
             <AiOutlineMenu className="Header-icon" size={25} onClick={handleClick}/>
             <p className="Header-title">Mercado</p>
             <AiOutlineUser className="Header-icon" size={25}/>
-        </header>
+        </TagHeader>
     )
 }
